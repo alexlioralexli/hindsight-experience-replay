dab20ab2b626de46c3183f37dd19a3c3851ab182
Alex Li <alexlioralexli@gmail.com>
2021-02-04 01:48:30 -0800

    fixed typo

--- experiment_scripts/2-4-polyak/2-4.py
+++ experiment_scripts/2-4-polyak/2-4.py
@@ -7,12 +7,12 @@ for env in envs:
         for type in ['--concatenate_fourier --train_B']:
             for fourier_dim in [256, 1024]:
                 for sigma in [0.01, 0.001]:
-                    commands.append(env + f' --network_class FourierMLP --sigma {sigma} --fourier_dim {fourier_dim} {type} --n_hidden 2 --polyak {polyak} &')
+                    commands.append(env + f' --network_class FourierMLP --sigma {sigma} --fourier_dim {fourier_dim} {type} --n_hidden 2 --polyak {polyak}')
     count = 0
     for command in commands:
         for seed in [10]:
             if total % 8 == 0:
                 print(total)
             total += 1
-            print(f'{command} --seed {seed}')
+            print(f'{command} --seed {seed} &')
 
